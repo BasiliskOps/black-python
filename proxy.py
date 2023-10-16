08618dba101addc6b327901d00d2f24a893e6b7c
BasiliskOps <noro.nero@gmail.com>
2023-10-16 14:38:58 -0500

    creation of handler functions

--- proxy.py
+++ proxy.py
@@ -25,3 +25,28 @@ def hexdump(src, length=16, show=True):
             print(line)
         else:
             return results
+        
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def proxy_handler(client_sockeet, remote_host, remote_port, receivee_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
